47c691fa9e656fa151758a61232bef62e89754bd
azampathan03 <azambilhaur@gmail.com>
2025-09-25 16:54:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from PIL import Image  
 
 
 # Constants
